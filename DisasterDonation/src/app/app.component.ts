380d5c77fc906842c44303d1536b84a5e243d249
dee-yeet <destin.yee343@gmail.com>
2019-11-02 16:31:52 -0400

    Added a title and example list element

--- DisasterDonation/src/app/app.component.ts
+++ DisasterDonation/src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { Charity } from './charity.model';
+import { Disaster } from './disaster.model';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +9,27 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'DisasterDonation';
+  public disasterArray = [new Disaster];
+  public charityDisplay: string = 'CHARITY';
+  public charityDisplayURL: string = '';
+  public showCharities: boolean = false;
+
+  public changeCharityButtonDisplay(charity: Charity): void {
+    this.charityDisplay = charity.name;
+    this.charityDisplayURL = charity.url;
+    this.displayCharity();
+  }
+
+  public displayCharity(): void {
+    this.showCharities = !this.showCharities;
+  }
+
+  public goToLink(): void {
+    if (this.charityDisplay != 'CHARITY') {
+      window.open(this.charityDisplayURL);
+    }
+  }
+
+
+
 }
---
+++ DisasterDonation/src/app/charity.model.ts
@@ -0,0 +1,9 @@
+export class Charity {
+    name: string;
+    url: string;
+
+    constructor(name: string, url: string) {
+        this.name = name;
+        this.url = url; 
+    }
+}
